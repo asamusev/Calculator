9cd0ac9860bc13a0d58f14d9db40dc40a944ed75
asamusev <asamusev2003@gmail.com>
2019-07-26 14:20:11 +0800

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import "dart:math";
 
 void main() => runApp(MyApp());
 
@@ -7,20 +8,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: calculate(),
+      home: Calculate(),
     );
   }
 }
 
-class calculate extends StatelessWidget {
-int x = 0;
-int y = 0;
-  
+class Calculate extends StatefulWidget {
+  @override
+  _CalculateState createState() => _CalculateState();
+}
+
+class _CalculateState extends State<Calculate> {
+String x ;
+String y ;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15),
+        padding: const EdgeInsets.symmetric(horizontal: 21),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -28,20 +34,29 @@ int y = 0;
           TextField(
             decoration: InputDecoration(labelText: '1 Number', prefixIcon: Icon(Icons.add_circle)),
           onChanged: (String n){
-              x = int.parse(n);
+              x = n;
             },
             ),
           TextField(
             decoration: InputDecoration(labelText: '2 Nubmer', prefixIcon: Icon(Icons.add_circle)),
           onChanged: (String n){
-            y = int.parse(n);
+            y = n;
           },
           ),
-          FlatButton(child: Text('multiply', style: TextStyle(fontSize: 26),), onPressed: () {
-            Navigator.push(context, CupertinoPageRoute(builder: (BuildContext context) => Resultats(y: y, x: x,)));
+          FlatButton(child: Text('X', style: TextStyle(fontSize: 26),), onPressed: () {
+            Navigator.push(context, CupertinoPageRoute(builder: (context) => Resultats(y: this.y, x: this.x, r: 1)));
           },),
-            FlatButton(child: Text('addition', style: TextStyle(fontSize: 26),), onPressed: () {
-            Navigator.push(context, CupertinoPageRoute(builder: (BuildContext context) => Resultats(y: y, x: x,)));
+            FlatButton(child: Text('+', style: TextStyle(fontSize: 26),), onPressed: () {
+            Navigator.push(context, CupertinoPageRoute(builder: (context) => Resultats(y: this.y, x: this.x, r: 2)));
+            },),
+            FlatButton(child: Text('-', style: TextStyle(fontSize: 26),), onPressed: () {
+            Navigator.push(context, CupertinoPageRoute(builder: (context) => Resultats(y: this.y, x: this.x, r: 3)));
+            },),
+            FlatButton(child: Text('/', style: TextStyle(fontSize: 26),), onPressed: () {
+            Navigator.push(context, CupertinoPageRoute(builder: (context) => Resultats(y: this.y, x: this.x, r: 4)));
+          },),
+          FlatButton(child: Text('x^', style: TextStyle(fontSize: 26),), onPressed: () {
+            Navigator.push(context, CupertinoPageRoute(builder:(context) => Resultats(y: this.y, x: this.x, r: 5)));
           },)
         ],
         ),
@@ -49,18 +64,54 @@ int y = 0;
     );
   }
 }
+class Resultats extends StatefulWidget {
+  final String x, y;
+  final int r;
+  String string;
+  @override
+  Resultats({this.x, this.y, this.r}) {
+    double n;
+    String s;
+    int res = this.r;
 
-class Resultats extends StatelessWidget {
-final int x;
-final int y;
-  const Resultats({Key key, this.x, this.y}) : super(key: key);
+    switch (res) {
+      case 1:
+        n = double.parse(this.x) * double.parse(this.y);
+        s = "X";
+        break;
+      case 2:
+        n = double.parse(this.x) + double.parse(this.y);
+        s = "+";
+        break;
+      case 3:
+        n = double.parse(this.x) - double.parse(this.y);
+        s = "-";
+        break;
+      case 4:
+        n = double.parse(this.x) / double.parse(this.y);
+        s = "/";
+        break;
+      case 5:
+        n = pow(double.parse(this.x), double.parse(this.y));
+        s = "^";
+        break;
+    }
+    string = this.x+" " + s+" " + this.y +" "+ "="+" " + n.toString();
+  }
+  _ResultatsState createState() => _ResultatsState();
+}
 
+class _ResultatsState extends State<Resultats> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(children: <Widget>[
-      Text('$x + $y = ${x+y}', style: TextStyle(fontSize: 26))
-      ],)
-    );
+        body: Row(
+         mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(this.widget.string, style: TextStyle(fontSize: 46),
+              )
+            ],
+          ), 
+         );
   }
 }
